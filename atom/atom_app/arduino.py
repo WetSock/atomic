eb466bb54f50db2233e9dd27b94f7a07bd0ec47b
wetsock <hommtoe@gmail.com>
2022-08-27 22:41:12 +0300

    Поменял работу с Arduino на асинхронную

--- atom/atom_app/arduino.py
+++ atom/atom_app/arduino.py
@@ -1,35 +1,81 @@
 from typing import List
 import aiohttp
 import asyncio
+from threading import Thread
 
 class Arduino():
     arduinoCommands = ["turnOff",
                         "setAqua",
                         "setYellow"]
+    def __init__(self):
+        self.demoThread = Thread(target=self.arduinoScenario, args=("Demo",))
+        self.demoThread1 = Thread(target=self.arduinoScenario, args=("Demo1",))
+        self.demoThread2 = Thread(target=self.arduinoScenario, args=("Demo2",))
+        self.demoThread3 = Thread(target=self.arduinoScenario, args=("Demo3",))
+        self.demoThread4 = Thread(target=self.arduinoScenario, args=("Demo4",))
+        self.demoThread5 = Thread(target=self.arduinoScenario, args=("Demo5",))
+        self.demoThread6 = Thread(target=self.arduinoScenario, args=("Demo6",))
+        self.demoThread7 = Thread(target=self.arduinoScenario, args=("Demo7",))
+        self.demoThread8 = Thread(target=self.arduinoScenario, args=("Demo8",))
+        self.demoThread9 = Thread(target=self.arduinoScenario, args=("Demo9",))
 
-    def arduinoScenario(self, scenario: int):
+
+    def arduinoScenario(self, scenario: str):
         
-        if scenario == 1:
+        if scenario == "Demo":
             asyncio.run(self.sendArduinoCommand("turnOff"))
-        if scenario == 2:
+        if scenario == "Demo1":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
             asyncio.run(self.sendArduinoCommand("setAqua"))
-        if scenario == 3:
+        if scenario == "Demo2":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo3":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo4":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo5":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo6":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo7":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo8":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
+            asyncio.run(self.sendArduinoCommand("setYellow"))
+        if scenario == "Demo9":
+            # Выключить всё, а затем включить нужные штуки
+            asyncio.run(self.sendArduinoCommand("turnOff"))
             asyncio.run(self.sendArduinoCommand("setYellow"))
         return
 
     async def sendArduinoCommand(self, command: str):
 
         if not (command in self.arduinoCommands):
-            return
+            return "Передана неизвестная команда для arduino"
 
         async with aiohttp.ClientSession() as session:
 
             requestURL = f'http://192.168.2.202/?{command}'
-            async with session.get(requestURL) as resp:
-                pokemon = await resp
-                print(pokemon)
+            try:
+                async with session.get(requestURL) as resp:
+                    result = await resp
+                    print(result)
+            except:
+                print("Arduino недоступен")
+                return
 
         
-arduino = Arduino()
-arduino.arduinoScenario(1)
-arduino.arduinoScenario(2)
--- atom/atom_app/views.py
+++ atom/atom_app/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-import requests
+from .arduino import Arduino
 
+arduinoClient = Arduino()
 
-
-def Demo(request):
-    ## x = requests.get('http://192.168.2.202/?turnOff')
+async def Demo(request):
+    arduinoClient.demoThread.start()
     return render(request, 'atom_app/Demo/index.html')
 
 def Scenario2(request):
@@ -20,39 +20,39 @@ def Scenario3(request):
 def Scenario4(request):
     return render(request, 'atom_app/Scenario4/index.html')
 
-def Demo1(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo1')
+async def Demo1(request):
+    arduinoClient.demoThread1.start()
     return render(request, 'atom_app/Scenario1/Demonstration/1.html')
 
 def Demo2(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo2')
+    arduinoClient.demoThread2.start()
     return render(request, 'atom_app/Scenario1/Demonstration/2.html')
 
 def Demo3(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo3')
+    arduinoClient.demoThread3.start()
     return render(request, 'atom_app/Scenario1/Demonstration/3.html')
 
 def Demo4(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo4')
+    arduinoClient.demoThread4.start()
     return render(request, 'atom_app/Scenario1/Demonstration/4.html')
 
 def Demo5(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo5')
+    arduinoClient.demoThread5.start()
     return render(request, 'atom_app/Scenario1/Demonstration/5.html')
 
 def Demo6(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo6')
+    arduinoClient.demoThread6.start()
     return render(request, 'atom_app/Scenario1/Demonstration/6.html')
 
 def Demo7(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo7')
+    arduinoClient.demoThread7.start()
     return render(request, 'atom_app/Scenario1/Demonstration/7.html')
 
 def Demo8(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo8')
+    arduinoClient.demoThread8.start()
     return render(request, 'atom_app/Scenario1/Demonstration/8.html')
 
 def Demo9(request):
-    ## x = requests.get('http://192.168.2.202/?setDemo9')
+    arduinoClient.demoThread9.start()
     return render(request, 'atom_app/Scenario1/Demonstration/9.html')
 
