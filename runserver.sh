a517fa2da1874412ca657521b728fad863209585
wetsock <hommtoe@gmail.com>
2022-09-01 23:47:27 +0300

    Запуск браузера чуть позже чтобы успел включиться джанго

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,5 @@
 cd /home/amadeo/Atomic/atomic/atom
 python3 manage.py runserver &
-google-chrome --kiosk http://127.0.0.1:8000 &
+sleep 2
+google-chrome --kiosk --disable-pinch --overscroll-history-navigation=0 http://127.0.0.1:8000 &
 disown
